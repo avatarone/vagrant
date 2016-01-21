d37f0a5d47623fcb6fbac7dc2e8c8792386549c1
Jonas Zaddach <zaddach@eurecom.fr>
2016-01-21 11:39:02 +0100

    Added initial machine for Avatar

---
+++ avatar/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+add-apt-repository -y ppa:team-gcc-arm-embedded/ppa
+apt-get update
+apt-get install -y git binutils-dev gettext flex bison pkg-config \
+    libglib2.0-dev nasm liblua5.1-0-dev libsigc++-2.0-dev \
+    texinfo gcc-arm-embedded expat libexpat1-dev python2.7-dev \
+    
+    
+mkdir projects
+cd projects && (
+    git clone --branch eurecom/avatar https://github.com/eurecom-s3/s2e.git
+    mkdir s2e-build
+    cd s2e-build && (
+        make -f ../s2e/Makefile
+    )
+    git clone --branch eurecom/wip https://github.com/eurecom-s3/gdb.git 
+    mkdir gdb-build
+    cd gdb-build && (
+        ../gdb/configure --with-python --with-expat=yes --target=arm-none-eabi
+    )
+    
+    
+)
