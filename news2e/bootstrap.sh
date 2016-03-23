15225d3f33ec1f3faa2d0cbd128605021296eb4b
Jonas Zaddach <zaddach@eurecom.fr>
2016-03-23 11:15:43 +0100

    Added building of debug qemu (needed because of a bug) and export of the S2E_DIR variable

--- news2e/bootstrap.sh
+++ news2e/bootstrap.sh
@@ -25,16 +25,28 @@ EOF
 
 mkdir projects
 cd projects
+
+# Check out the source code
 git clone https://github.com/eurecom-s3/news2e
+
+# Build the source code
 mkdir news2e-build
 ( cd news2e-build && make -f ../news2e/Makefile )
+
 # There is a bug in compiler-rt which reports a missing features.h.
 # Weirdly enough, it's simply enough to restart the build.
 ( cd news2e-build/llvm-native && make )
 ( cd news2e-build && make -f ../news2e/Makefile )
+
 # The test repository does not check out through the build script
 ( cd news2e && git submodule update --init test )
 
+# There is a bug which manifests only in the release build of qemu.
+# Until we have fixed that, use the debug build.
+( cd news2e-build && make -f ../news2e/Makefile stamps/qemu-debug-make )
+
+# Add export for S2E_DIR to profile
+echo "export S2E_DIR=$(pwd)/news2e-build/qemu-debug" >> $HOME/.profile
 
 
 
