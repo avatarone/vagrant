e387b1ed33f1ff8b7ca8c461402ea082649b4c31
Dario Nisi <nisi@eurecom.fr>
2016-08-09 16:19:48 +0200

    Checkout the newklee branch on bootstrap

--- news2e/bootstrap.sh
+++ news2e/bootstrap.sh
@@ -178,21 +178,15 @@ cd projects
 # Check out the source code
 git clone https://github.com/eurecom-s3/news2e
 
+#Checkout to jonas/newklee branch
+( cd news2e && git fetch && git checkout jonas/newklee && git submodule update )
+
 # Build the source code
 mkdir news2e-build
 ( cd news2e-build && make -f ../news2e/Makefile )
 
-# There is a bug in compiler-rt which reports a missing features.h.
-# Weirdly enough, it's simply enough to restart the build.
-( cd news2e-build/llvm-native && make )
-( cd news2e-build && make -f ../news2e/Makefile )
-
 # The test repository does not check out through the build script
 ( cd news2e && git submodule update --init test )
-
-# There is a bug which manifests only in the release build of qemu.
-# Until we have fixed that, use the debug build.
-( cd news2e-build && make -f ../news2e/Makefile stamps/qemu-debug-make )
 
 # Add export for S2E_DIR to profile
 echo "export S2E_DIR=$(pwd)/news2e-build/qemu-debug" >> $HOME/.profile
